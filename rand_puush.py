0e21028a8bed60019d546eaa6e63403a57410683
Maxime Orione <maxpower98@hotmail.com>
2017-06-10 12:32:02 +0200

    Added some comments

--- rand_puush.py
+++ rand_puush.py
@@ -1,6 +1,8 @@
 import urllib.request, webbrowser, string, random, sys
 
 def generate_link():
+	# Generate link with random string 
+	# https://puu.sh/[5 characters]/filename.png
     return "https://puu.sh/" + ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=5)) + "/" + "filename_doesnt_matter.png"
 
 def get_request(url):
@@ -8,16 +10,19 @@ def get_request(url):
 
 def check_link(req):
     try:
+    	# Fetch html from url, may throw an HTTPError (Forbidden)
         html = urllib.request.urlopen(req).read()
-        # Check if it's a usable url
+        # Check if it's a usable url, if so, we return it
         if 'That puush could not be found.' not in str(
                 html) and 'You do not have access to view that puush.' not in str(html):
             return html
     except urllib.error.HTTPError:
-        pass
+        pass 
+    # If it wasn't a usable url, return false
     return False
 
 def update_console(found, generated):
+	# Cool dynamic display of found and generated links
     sys.stdout.write("\r{0}".format("Found "+str(found)+" out of "+str(generated)+" links"))
     sys.stdout.flush()
     return
@@ -57,6 +62,7 @@ while len(links) < n:
     # Update console
     update_console(len(links), i)
 print("\nLinks:")
+# Display all links
 for x in links:
     print(x)
 print("Opening links...")
